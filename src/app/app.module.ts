c5980af8fc8e5f4cb034ef3b677df1052c8d02a5
Marvin Nyingi <nyingi.marvin@outlook.com>
2024-09-09 23:18:13 +0300

    9/9/2024

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,21 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { CategoryComponent } from './admin/master/category/category.component';
 import { ProductComponent } from './admin/master/product/product.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
-  declarations: [AppComponent, CategoryComponent, ProductComponent],
+  declarations: [AppComponent, CategoryComponent, ProductComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatToolbarModule, MatButtonModule, MatIconModule,MatSidenavModule,MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent],
